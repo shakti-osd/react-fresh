408eee8c29fba2170db9b5062b9eb9480e3fcbb0
Shakti Kumar <shakti.opensourcedeveloper@gmail.com>
2024-05-10 13:59:52 +0530

    Updated homepage message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 class App extends Component {
   render() {
@@ -8,14 +8,15 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>CICD Using Github Runner</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Created Pipeline using Github Runner, and hosted on AWS Using Nginx
+          Server.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
